713286c101bf362b5f0607917006f514e7041c75
Mayen Enodien <menodien@gmail.com>
2021-06-01 10:11:34 +0100

    Add name to Service

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,102 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying all services!"
+Service.destroy_all
+
+puts "Creating 15 services..."
+
+Service.create!(
+  title: 'Deep tissue massage',
+  address: Faker::Address.street_address,
+  price: Faker::Commerce.price
+)
+
+Service.create!(
+  title: 'Hair styling, hair cuts',
+  address: Faker::Address.street_address,
+  price: Faker::Commerce.price
+)
+
+Service.create!(
+  title: 'Spray tanning and sunless tanning',
+  address: Faker::Address.street_address,
+  price: Faker::Commerce.price
+)
+
+Service.create!(
+  title: 'Cellulite treatments',
+  address: Faker::Address.street_address,
+  price: Faker::Commerce.price
+)
+
+Service.create!(
+  title: 'Colonic Hydrotherapy',
+  address: Faker::Address.street_address,
+  price: Faker::Commerce.price
+)
+
+Service.create!(
+  title: 'Manicure',
+  address: Faker::Address.street_address,
+  price: Faker::Commerce.price
+)
+
+Service.create!(
+  title: 'Pedicure',
+  address: Faker::Address.street_address,
+  price: Faker::Commerce.price
+)
+
+Service.create!(
+  title: 'Hydrafacial',
+  address: Faker::Address.street_address,
+  price: Faker::Commerce.price
+)
+
+Service.create!(
+  title: 'Laser skin rejuvenation',
+  address: Faker::Address.street_address,
+  price: Faker::Commerce.price
+)
+
+Service.create!(
+  title: 'Hair extensions',
+  address: Faker::Address.street_address,
+  price: Faker::Commerce.price
+)
+
+Service.create!(
+  title: 'Foot massage',
+  address: Faker::Address.street_address,
+  price: Faker::Commerce.price
+)
+
+Service.create!(
+  title: 'Colonic Hydrotherapy',
+  address: Faker::Address.street_address,
+  price: Faker::Commerce.price
+)
+
+Service.create!(
+  title: 'Body exfoliation treatments',
+  address: Faker::Address.street_address,
+  price: Faker::Commerce.price
+)
+
+Service.create!(
+  title: 'Body waxing treatments',
+  address: Faker::Address.street_address,
+  price: Faker::Commerce.price
+)
+
+Service.create!(
+  title: 'Laser hair removal',
+  address: Faker::Address.street_address,
+  price: Faker::Commerce.price
+)
+
+puts "All done"
+
+
