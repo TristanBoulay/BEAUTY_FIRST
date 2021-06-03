6bcb692cd4c1f7f4ce1ea751a030e3e42d109f8b
tristanboulay <tristanboulay.casa@gmail.com>
2021-06-03 12:47:52 +0100

    markers on map

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -14,6 +14,14 @@ class ServicesController < ApplicationController
 
   def show
     @service = Service.find(params[:id])
+    @markers = [
+      {
+        lat: @service.latitude,
+        lng: @service.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { service: @service }),
+        # image_url: helpers.asset_url('logo.png')
+      }
+    ]
   end
 
 
