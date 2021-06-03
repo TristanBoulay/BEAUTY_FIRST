ea1c3a309647fd20f07077f4c408881932e19634
Mayen Enodien <menodien@gmail.com>
2021-06-03 11:41:01 +0100

    completed search bar

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,12 +1,16 @@
 class ServicesController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
   def index
-    @services = Service.all
+    if params[:query].present?
+      @services = Service.search_by_name_and_title_and_address(params[:query])
+    else
+      @services = Service.all
+    end
     @markers = @services.geocoded.map do |service|
       {
         lat: service.latitude,
         lng: service.longitude,
-        info_window: render_to_string(partial: "info_window", locals: { service: service }),
+        info_window: render_to_string(partial: "info_window", locals: { service: service })
         # image_url: helpers.asset_url('logo.png')
       }
     end
