a5e8f5f8f53561178c55cf1a9eea0dc8a72011cd
Johann Alerte <johann.alerte@gmail.com>
2021-06-04 16:40:57 +0100

    fixes

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,8 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :service
 
-    validates :comment, presence: true
-    validates :comment, length: { minimum: 6 }
+  validates :comment, presence: true
+  validates :comment, length: { minimum: 6 }
+  validates :rating, presence: true
 
 end
